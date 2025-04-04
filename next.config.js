559341e50a42d9447bdf24e5bad5100cdfd604fd
Gagandeep.dhaliwal <gagandeep.dhaliwal2@student.ufv.ca>
2025-04-03 21:11:22 -0700

    Update next.config.js with HTTPS configuration and correct basePath

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/Portfolio',
+  basePath: process.env.NODE_ENV === 'production' ? '/temp-portfolio' : '',
   images: {
     unoptimized: true,
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://gagandhaliwal.github.io/temp-portfolio' : '',
 }
 
 module.exports = nextConfig 
