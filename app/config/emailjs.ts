029b953c547352a22f2b39fae24ec41b96fcee09
Gagandeep.dhaliwal <gagandeep.dhaliwal2@student.ufv.ca>
2025-04-05 15:49:43 -0700

    Add EmailJS configuration for contact form

---
+++ app/config/emailjs.ts
@@ -0,0 +1,10 @@
+export const emailjsConfig = {
+  // You can find this in Email Services after creating your Gmail service
+  serviceId: 'service_5zliaqh',  // Your Gmail service ID
+  
+  // You can find this in Email Templates after saving your template
+  templateId: 'template_m83evaq',  // Your template ID
+  
+  // You can find this in Account -> API Keys
+  publicKey: 'jrtW25p4QyAx1ubOc',  // Your public key
+}; 
